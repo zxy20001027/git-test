6269f3d82a0b367a222a1c868608aba921fa52a8
Layne <Layne@atguigu.com>
2022-06-27 00:55:04 +0800

    second commit

--- src/main/java/com/hy/git/GitTest.java
+++ src/main/java/com/hy/git/GitTest.java
@@ -3,5 +3,6 @@ package com.hy.git;
 public class GitTest {
     public static void main(String[] args){
         System.out.println("hello.git!");
+        System.out.println("hello.git2!");
     }
 }
